91aa0d1575bda377b5dc3ee0f45bf15cf0412734
NaveenG <naveenkumar.gundubilli@gmail.com>
2015-09-22 22:01:38 +1000

    Added Exception class

---
+++ PrintJobCost/src/main/java/com/print/exception/PrintException.java
@@ -0,0 +1,10 @@
+package com.print.exception;
+
+public class PrintException extends Exception{
+
+	private static final long serialVersionUID = 8141722868239545009L;
+
+	public PrintException(String string) {
+        super(string);
+    }
+}
