8bcddbcbcf2411a161483e00ab239f4753033217
NaveenG <naveenkumar.gundubilli@gmail.com>
2015-09-22 16:05:59 +1000

    Changed Package

---
+++ PrintJobCost/src/main/java/com/codetest/print/PrintJobParameters.java
@@ -0,0 +1,33 @@
+package com.codetest.print;
+
+public class PrintJobParameters {
+
+	private String numberOfPages;
+	private String numberOfColorPages;
+	private boolean isDoubleSided;
+
+	public String getNumberOfPages() {
+		return numberOfPages;
+	}
+
+	public void setNumberOfPages(String numberOfPages) {
+		this.numberOfPages = numberOfPages;
+	}
+
+	public String getNumberOfColorPages() {
+		return numberOfColorPages;
+	}
+
+	public void setNumberOfColorPages(String numberOfColorPages) {
+		this.numberOfColorPages = numberOfColorPages;
+	}
+
+	public boolean isDoubleSided() {
+		return isDoubleSided;
+	}
+
+	public void setDoubleSided(boolean isDoubleSided) {
+		this.isDoubleSided = isDoubleSided;
+	}
+
+}
